47b5010a5a576609f8c065fc89ab8155da9e7522
Arafat Hossain <arafat6462@gmail.com>
2025-07-13 13:42:29 +0600

    Add owner field to Project model and create migration

--- api/models.py
+++ api/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Project(models.Model):
+    owner = models.ForeignKey(User, related_name='projects', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
