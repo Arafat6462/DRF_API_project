2ae063689b987465e55e0f098ca603ce68f44b4e
Arafat Hossain <arafat6462@gmail.com>
2025-07-13 13:57:27 +0600

    Implement project ownership permissions and restrict access to authenticated users

--- api/views.py
+++ api/views.py
@@ -2,7 +2,8 @@ from rest_framework import viewsets, generics
 from .models import Project
 from .serializers import ProjectSerializer, UserSerializer
 from django.contrib.auth.models import User
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .permissions import IsOwnerOrReadOnly
 
 
 class RegisterView(generics.CreateAPIView):
@@ -14,3 +15,10 @@ class RegisterView(generics.CreateAPIView):
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        return self.request.user.projects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
