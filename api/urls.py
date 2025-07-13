bc43371481ed73d1dcc60d7986870bd82dbaa1b1
Arafat Hossain <arafat6462@gmail.com>
2025-07-13 13:10:01 +0600

    Add user registration functionality with JWT authentication support

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,11 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import ProjectViewSet
+from .views import ProjectViewSet, RegisterView
 
 router = DefaultRouter()
 router.register(r'projects', ProjectViewSet, basename='project')
 urlpatterns = router.urls
+
+urlpatterns += [
+    path('auth/register/', RegisterView.as_view(), name='auth_register'),
+]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .models import Project
-from .serializers import ProjectSerializer
+from .serializers import ProjectSerializer, UserSerializer
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = UserSerializer
+
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
